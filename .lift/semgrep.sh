1ee7217d6bc4cc552422300b4d45092b51b81cd8
Thomas M. DuBuisson <tommd@muse.dev>
2021-06-11 12:36:29 -0700

    .

---
+++ .lift/semgrep.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+function tellApplicable() {
+    # By default semgrep rules include only go, python, java, and javascript
+    files=$(git ls-files | grep -E '.js|.java|.py|.go$' | head)
+    res="broken"
+    if [[ -z "$files" ]] ; then
+        res="false"
+    else
+        res="true"
+    fi
+    printf "%s\n" "$res"
+}
+
+function tellVersion() {
+    echo "1"
+}
+
+function tellName() {
+    echo "Semgrep"
+}
+
+function emit_results() {
+  echo "$1" | \
+    jq '[.results | .[] | .line = .end.line | .file = .path | .message = .extra.message | .type = .check_id | .cwe = .extra.metadata.cwe | del(.extra) | del(.start) | del (.end) | del(.path) | del(.check_id) ]'
+}
+
+function run() {
+    # Semgrep can blow up so we limit it to 10 minutes
+    pip3 install --upgrade semgrep
+    raw_results=$(timeout 10m semgrep --disable-version-check --json --config /opt/semgrep/semgrep-rules.yaml)
+    result=$?
+    if [[ $result = 0 ]] ; then
+        emit_results "$raw_results"
+    else
+        printf "Tool timed out!" >&2
+        exit $result
+    fi
+}
+
+case "$3" in
+    run)
+        run
+        ;;
+    applicable)
+        tellApplicable
+        ;;
+    name)
+        tellName
+        ;;
+    *)
+        tellVersion
+        ;;
+esac
--- garbage.js
+++ garbage.js
@@ -1,59 +1,43 @@
 const sql = "select * from * where * = ?";
 let garbage;
-​
 garbage++;
-​
 const garbage = 2; //twice defined?
-​
 sql = 3; // can't reassign a const variable
-​
 console.log(garbage);
-​
 console.log("2" + "1");  // => "21"
 console.log("2" - "1");  // => 1
-​
 console.log('' == 0);    // => true
-​
 console.log(true == []); // -> false
 console.log(true == ![]); // -> false
-​
 function getProp(object, propertyName, defaultValue) {
   if (!object[propertyName]) {
     return defaultValue;
   }
   return object[propertyName];
 }
-​
 const hero = {
   name: 'Batman',
   isVillian: false
 };
-​
 console.log(getProp(hero, 'name', 'Unknown'));     // => 'Batman'
-​
 console.log(getProp(hero, 'isVillian', true)); // => true
-​
 function getPropFixed(object, propertyName, defaultValue) {
    if (object[propertyName] === undefined) {
      return defaultValue;
    }
    return object[propertyName];
 }
-​
 const hero = {
   name: 'Batman',
   isVillian: false
 };
-​
 console.log(getPropFixed(hero, 'isVillian', true)); // => false
-​
 function getPropFixedBetter(object, propertyName, defaultValue) {
    if (!(propertyName in object)) {
      return defaultValue;
    }
    return object[propertyName];
 }
-​
 function someFunc(array) {
   var index, item, length = array.length;
   /*
@@ -65,7 +49,6 @@ function someFunc(array) {
   }
   return someResult;
 }
-​
 // Bad
 let message;
 // ...
@@ -73,15 +56,12 @@ if (notFound) {
   message = 'Item not found';
   // Use `message`
 }
-​
 let count;
 console.log(count); // => undefined
-​
 const hero = {
   name: 'Batman'
 };
 console.log(hero.city); // => undefined
-​
 // Bad
 const object = {
   prop: 'value'
@@ -89,7 +69,6 @@ const object = {
 if (object.nonExistingProp === undefined) {
   // ...
 }
-​
 // Bad
 function foo(param1, param2) {
   if (param2 === undefined) {
